8ecc8b2423426a2c6ad88569ee2f2235d5690c99
Filip Rybiński <rybciaw77@gmail.com>
2024-04-17 21:46:22 +0200

    Init

---
+++ src/Program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AiTSI");
+        }
+    }
+}
---
+++ src/resources/Analyzer/Analyzer.cs
@@ -0,0 +1,5 @@
+﻿namespace Analyzer;
+
+public class Analyzer
+{
+}
